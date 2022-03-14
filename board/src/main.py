052e9aa89cb73cac19d38ca76dcfaa2bb6f75470
WiCoKo <wmconnorkoch@gmail.com>
2022-03-14 09:35:16 -0700

    Added angular front end
    Preliminary angular webapp front end, beta phase

---
+++ board/src/main.py
@@ -0,0 +1,8 @@
+from multiprocessing.connection import wait
+
+
+import time
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(1)
---
+++ webapp/angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { PixelButtonComponent } from './pixel-button/pixel-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent,
+    PixelButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
