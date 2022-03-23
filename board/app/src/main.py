637416b53db0f793dcc06034493f3b7b1c9cd0fb
WiCoKo <wmconnorkoch@gmail.com>
2022-03-23 00:53:28 -0700

    Updated python files to conform to pylint

--- board/app/src/main.py
+++ board/app/src/main.py
@@ -1,11 +1,12 @@
-from multiprocessing.connection import wait
-
+"""
+Main module of the community board controller
+"""
 import os
 import sys
 import time
 import traceback
-import pymongo
 import logging
+import pymongo
 from utils import get_module_logger
 # from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
@@ -16,36 +17,38 @@ MONGO_PASS = os.getenv('MONGO_PASS')
 BOARD_DB_NAME = os.getenv('MONGO_DB_NAME')
 BOARD_COLLECTION_NAME = 'boardMapping'
 MONGO_HOST = os.getenv('MONGO_CONTAINER')
-MONGO_URI = 'mongodb://' + MONGO_USER + ':' + MONGO_PASS + '@' + MONGO_HOST + ':27017/?authSource=admin' # Create the mongo URI that will be used by the client
-
-
+# Create the mongo URI that will be used by the client
+MONGO_URI = 'mongodb://'+MONGO_USER+':'+MONGO_PASS+'@'+MONGO_HOST+':27017/?authSource=admin'
 
 
 def run():
-
+    '''
+    The main function of the community board driver,
+    initializes the board and launches into the changestream rendering loop
+    '''
     LOGGER.info("Beginning Initial Setup")
-    mongoClient = pymongo.MongoClient(MONGO_URI)
-    boardDb = mongoClient[BOARD_DB_NAME]
-    boardCollection = boardDb[BOARD_COLLECTION_NAME]
+    mongo_client = pymongo.MongoClient(MONGO_URI)
+    board_db = mongo_client[BOARD_DB_NAME]   
+    board_collection =   board_db[BOARD_COLLECTION_NAME]
 
     # TODO: Check for null values on constants
     # Try to connect to the mongodb database
-    for x in range(0,4):
+    for _ in range(0,4):
         time.sleep(2)
         try:
-            serverInfo = mongoClient.server_info()
-        except pymongo.errors.ServerSelectionTimoutError as err:
+            server_info = mongo_client.server_info()
+        except pymongo.errors.ServerSelectionTimeoutError as err:
             LOGGER.error(err)
             sys.exit()
 
         else:
-            LOGGER.info('Connected to MongoDB at: ' + MONGO_HOST)
-            LOGGER.debug('Database server information:  ' + str(serverInfo))
+            LOGGER.info('Connected to MongoDB at: %s', MONGO_HOST)
+            LOGGER.debug('Database server information: %s', str(server_info))
             break
-    
+
     # Query for first and last index of collection
-    LOGGER.debug('First Entry:' + str(boardCollection.find_one({'index':0})))
-    LOGGER.debug('Last Entry: ' + str(boardCollection.find_one({'index':2047})))
+    LOGGER.debug('First Entry: %s', str(board_collection.find_one({'index':0})))
+    LOGGER.debug('Last Entry: %s', str(board_collection.find_one({'index':2047})))
 
 
     LOGGER.info("Beginning Board Rendering Loop")
@@ -54,11 +57,12 @@ def run():
         # TODO: Work in MONGODB polling to keep board up to date, maybe look into changestreams
         # TODO: Begin testing in RPI remote docker env
         time.sleep(1)
+    return()
 
 
 if __name__ == '__main__':
     try:
         run()
-    except Exception as e:
+    except KeyboardInterrupt as e:
         logging.error(traceback.format_exc())
         sys.exit(e)
--- board/app/src/utils.py
+++ board/app/src/utils.py
@@ -1,21 +1,21 @@
-from multiprocessing.connection import wait
-
-import sys
-import time
+'''Utils module for supplemetal functions'''
 import logging
 import os
 
 def get_file(path):
-    dir = os.path.dirname(os.path.dirname(__file__))
-    return os.path.join(dir, path)
+    ''' Get a specific file using the path'''
+    directory = os.path.dirname(os.path.dirname(__file__))
+    return os.path.join(directory, path)
 
 # Function to get the logger for a specific module: do logger = get_module_logger(__name__)
 def get_module_logger(mod_name):
+    ''' Gets an appropriate logger to handle the module'''
     logger = logging.getLogger(mod_name)
     handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s') # ISO-8601 Format for timestamp
+    # ISO-8601 Format for timestamp
+    formatter = logging.Formatter('%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    logger.setLevel(os.environ.get('LOG_LEVEL') or 'INFO') # Default to INFO if LOG_LEVEL environment variable is not defined
+    # Default to INFO if LOG_LEVEL environment variable is not defined
+    logger.setLevel(os.environ.get('LOG_LEVEL') or 'INFO')
     return logger
-
